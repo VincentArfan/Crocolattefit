90e40c1c1304f387cbfaded4f9272ec9fb6c7a32
Ricardo231112400 <ricardo@students.mikroskil.ac.id>
2025-06-25 21:08:47 +0700

    Membuat halaman News

---
+++ lib/screens/News.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/news_provider.dart';
+
+class News extends StatelessWidget {
+  const News({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final newsProvider = Provider.of<NewsProvider>(context);
+    final newsList = newsProvider.newsList;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("News"),
+        backgroundColor: Colors.teal,
+        titleTextStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+        ),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(8),
+        itemCount: newsList.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 8,
+          mainAxisSpacing: 8,
+          childAspectRatio: 0.75,
+        ),
+        itemBuilder: (context, index) {
+          final news = newsList[index];
+          return Card(
+            color: Colors.teal[50],
+            elevation: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.network(
+                  news.imageUrl,
+                  height: 100,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    news.title,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Text(
+                    news.description,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
